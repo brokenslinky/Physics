f23b86d288b753c6ac5e6ca13aefa80372987c79
Brock Palmer <brokenslinky@gmail.com>
2019-08-18 20:00:08 -0700

    Add CelestialBody Particle

--- Physics/Objects.cs
+++ Physics/Objects.cs
@@ -33,4 +33,23 @@ namespace Physics
         }
         public void Iterate(Time timeStep) { Iterate(timeStep, new Force(0.0, 0.0, 0.0)); }
     }
+
+    public class CelestialBody : Particle
+    {
+        // <desription> A CelestialBody is a Particle with 
+        // constructors to calculate position and momentum 
+        // given a phase angle and orbital properties. </description>
+
+        double Mu = 1.327124400189 * Math.Pow(10, 20);
+
+        public CelestialBody(double mass, double periapsis, double apoapsis, double phaseAngle = 0.0)
+        {
+            this.mass = new Mass(mass);
+            // just calculate at apoapsis for now
+            this.position = new Position(apoapsis * Math.Cos(phaseAngle), apoapsis * Math.Sin(phaseAngle), 0.0);
+            double momentumAtApoapsis = mass * Math.Sqrt(Mu * (2.0 / apoapsis - 2.0 / (apoapsis + periapsis)));
+            this.momentum = new Momentum(-momentumAtApoapsis * Math.Sin(phaseAngle),
+                momentumAtApoapsis * Math.Cos(phaseAngle), 0.0);
+        }
+    }
 }
